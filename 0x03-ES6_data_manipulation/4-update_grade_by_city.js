52d5c9131ccedbac5b8580e51a30b4db74b352f8
chaimae <chaimaeaafif@gmail.com>
2024-10-08 22:34:13 +0000

    update

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByCity(arr,city,newGrades) {
+  for (const grade of newGrades){
+    for (const student of arr){
+      if (student.id === grade.studentId){
+        studnt.grade = grade.grade;
+      }
+    }
+  }
+  return arr.filter((val) => val.location === city).map((val) => {
+    if (!("grade" in val)){
+      const newVal = val;
+      newVal.grade = "N/A";
+      return newVal;
+    }
+    return val;
+  });
+}
