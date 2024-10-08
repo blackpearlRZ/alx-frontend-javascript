d10ea04aa55553e1d48203ddcf8cf3601d958fc5
chaimae <chaimaeaafif@gmail.com>
2024-10-08 22:55:16 +0000

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,15 @@
-export default function updateStudentGradeByCity(arr,city,newGrades) {
-  for (const grade of newGrades){
-    for (const student of arr){
-      if (student.id === grade.studentId){
-        studnt.grade = grade.grade;
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  for (const grade of newGrades) {
+    for (const student of arr) {
+      if (student.id === grade.studentId) {
+        student.grade = grade.grade;
       }
     }
   }
   return arr.filter((val) => val.location === city).map((val) => {
-    if (!("grade" in val)){
+    if (!('grade' in val)) {
       const newVal = val;
-      newVal.grade = "N/A";
+      newVal.grade = 'N/A';
       return newVal;
     }
     return val;
