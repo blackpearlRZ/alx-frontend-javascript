8540cf28fda44a460f3fab10c96a97487e189ddb
chaimae <chaimaeaafif@gmail.com>
2024-10-11 22:41:36 +0000

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const groceriesListMap = new Map();
+  groceriesListMap.set('Apples', 10);
+  groceriesListMap.set('Tomatoes', 10);
+  groceriesListMap.set('Pasta', 1);
+  groceriesListMap.set('Rice', 1);
+  groceriesListMap.set('Banana', 5);
+  return groceriesListMap;
+}
