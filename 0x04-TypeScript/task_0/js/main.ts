17a5230314d769d8ff9287a2e19a9946c98bc5ea
chaimae <chaimaeaafif@gmail.com>
2024-10-11 23:09:09 +0000

    task0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,47 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+firstName: 'Abdulrahman',
+lastName: 'Metawei',
+age: 30,
+location: 'Cairo, Egypt',
+};
+
+const student2: Student = {
+firstName: 'John',
+lastName: 'Doe',
+age: 38,
+location: 'Atlanta, USA',
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table') as HTMLTableElement;
+const headerRow = document.createElement('tr') as HTMLTableRowElement;
+const rows: string[] = ['First Name', 'Location'];
+rows.map((value: string) => {
+const th = document.createElement('th') as HTMLElement;
+th.textContent = value as string;
+headerRow.appendChild(th);
+});
+table.appendChild(headerRow);
+studentsList.map((value: Student) => {
+const row = document.createElement('tr') as HTMLTableRowElement;
+const th1 = document.createElement('th') as HTMLElement;
+const th2 = document.createElement('th') as HTMLElement;
+th1.textContent = value.firstName;
+th2.textContent = value.location;
+row.appendChild(th1);
+row.appendChild(th2);
+table.appendChild(row);
+})
+document.body.appendChild(table);
+
+// studentsList.map((value: Student): void => {
+
+// });
